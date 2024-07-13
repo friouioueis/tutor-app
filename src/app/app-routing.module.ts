250499324b3866ceb2842a7b85d3ae0fe4732885
unknown <go_frioui@esi.dz>
2024-07-13 12:47:37 +0100

    Fixed video

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { AppComponent } from './app.component';
 
 const routes: Routes = [
-  { path: '', component: AppComponent },
+  { path: '', component: HomeComponent },
 ];
 
 @NgModule({
